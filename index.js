95834eda0fdd297c4a6306113957a534893c2303
kamayatu <sarubeji15@gmail.com>
2023-03-25 15:20:06 +0900

    resolved error

--- index.js
+++ index.js
@@ -1,39 +1,49 @@
 const express = require('express');
 const multer = require('multer');
-const uuidv4 = require('uuid/v4');
+// const uuidv4 = require('uuid/v4');
+const { v4:uuidv4} = require('uuid');
+uuidv4();
 
+const app = express(); // expressアプリを生成する
+app.use(multer().none()); // multerでブラウザから送信されたデータを解釈する
+app.use(express.static('web')); // webフォルダの中身を公開する
 
-const app = express();
-app.use(multer().none());
-app.use(express.static('web'));
-
+// TODOリストデータ
 const todoList = [];
 
+// http://localhost:3000/api/v1/list にアクセスしてきたときに
+// TODOリストを返す
 app.get('/api/v1/list', (req, res) => {
-  res.json(todoList);
+    // JSONを送信する
+    res.json(todoList);
 });
 
-app.get('/api/v1/list', (req, res) => {
-  const todoData = req.body;
-  const todoTitle = todoData.title;
+// http://localhost:3000/api/v1/add にデータを送信してきたときに
+// TODOリストに項目を追加する
+app.post('/api/v1/add', (req, res) => {
+    // クライアントからの送信データを取得する
+    const todoData = req.body;
+    const todoTitle = todoData.title;
+    
+    // ユニークIDを生成する
+    const id = uuidv4();
 
-  const id = uuidv4()
-  const todoItem = {
-    id,
-    title: todoTitle,
-    done: false
-  };
+    // TODO項目を作る
+    const todoItem = {
+        id,
+        title: todoTitle,
+        done: false
+    };
 
-  todoList.push(todoItem);
-  console.log('Add: ' + JSON.stringify(todoItem));
-  // const todoList = [
-  //   { title: 'JavaScriptを勉強する', done: true},
-  //   { title: 'Node.jsを勉強する', done: false},
-  //   { title: 'Web APIを作る', done: false}
-  // ];
-  res.json(todoItem);
+    // TODOリストに項目を追加する
+    todoList.push(todoItem);
+
+    // コンソールに出力する
+    console.log('Add: ' + JSON.stringify(todoItem));
+
+    // 追加した項目をクライアントに返す
+    res.json(todoItem);
 });
 
-
+// ポート3000でサーバを立てる
 app.listen(3000, () => console.log('Listening on port 3000'));
-
